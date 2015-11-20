e99c1e5679cc74c8cb71385ea6e46bbb5d9a358e
Alejandro <alejandro.cabanas-rojas@amido.com>
2015-11-20 10:44:53 +0000

    Maven multi module example

---
+++ HLUnsubscribe/src/main/java/com/hendrix/lambdas/unsubscribe/App.java
@@ -0,0 +1,13 @@
+package com.hendrix.lambdas.unsubscribe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
